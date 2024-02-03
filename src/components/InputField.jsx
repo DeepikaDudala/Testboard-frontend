72fccfd608530634edb651b2ff9e292be6da75d6
Deepika Siva Sahithi Dudala <128839850+DeepikaDudala@users.noreply.github.com>
2024-02-04 01:32:50 +0530

    Update InputField.jsx

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,16 +1,14 @@
 function InputField({ type, place, id, name, value, handleChange }) {
   return (
-    <div>
-      <input
-        type={type}
-        placeholder={place}
-        id={id}
-        name={name}
-        value={value}
-        onChange={handleChange}
-        className="input-bottom-border my-3 p-2 w-50"
-      />
-    </div>
+    <input
+      type={type}
+      placeholder={place}
+      id={id}
+      name={name}
+      value={value}
+      onChange={handleChange}
+      className="input-bottom-border my-3  w-50 "
+    />
   );
 }
 
