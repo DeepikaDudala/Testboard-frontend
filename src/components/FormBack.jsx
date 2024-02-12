6abf9f62654524bfa8bbc2019493da750e81083c
Deepika Siva Sahithi Dudala <128839850+DeepikaDudala@users.noreply.github.com>
2024-02-12 19:11:09 +0530

    Update FormBack.jsx

--- src/components/FormBack.jsx
+++ src/components/FormBack.jsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React from 'react';
 
 function FormBack({ heading, img, form }) {
-  return (
-    <>
-      <div className=" pt-3 mt-3 text-center">
-        <div className="row">
-          <div className="col-md-5  d-none d-md-inline">
-            <img src={img} alt="Login Logo" />
-          </div>
-          <div className="col-md-7 ">
-            <h4 className="text-primary">{heading}</h4>
-            {form}
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className=" pt-3 mt-3 text-center">
+				<div className="row">
+					<div className="col-md-5  d-none d-md-inline">
+						<img src={img} alt="Login Logo" />
+					</div>
+					<div className="col-md-7 ">
+						<h4 className="my-3 text-primary">{heading}</h4>
+						{form}
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default FormBack;
