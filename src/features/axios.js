56aaf9d8655221233eee3bf11ff8c1c46f3e5bc2
Deepika Siva Sahithi Dudala <128839850+DeepikaDudala@users.noreply.github.com>
2024-03-11 09:54:25 +0530

    Update axios.js

--- src/features/axios.js
+++ src/features/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 axios.defaults.withCredentials = "include";
 const instance = axios.create({
-  baseURL: 'https://testboard-api-v1.onrender.com/api/v1',
+  baseURL: 'https://testboard-api.onrender.com/api/v1',
   withCredentials: "include",
 });
 
